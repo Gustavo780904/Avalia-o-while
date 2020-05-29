85c5f48559f6fd09916919035b84eefab2d2ef15
Gustavo780904 <gustavodutrasantos@gmail.com>
2020-05-29 19:35:56 -0300

    Menu repetindo

--- src/avaliacao/AvaliacaoWhile.java
+++ src/avaliacao/AvaliacaoWhile.java
@@ -9,7 +9,7 @@ public class AvaliacaoWhile {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
-		int opcao;
+		int opcao = 0;
 		String nome;
 		double horasTrabalhadas = 0.0, valorHora = 0.0;
 		char digitarOutro;
@@ -28,6 +28,17 @@ public class AvaliacaoWhile {
 			System.out.print("Digitar outro (S/N)? ");
 			digitarOutro = sc.next().charAt(0);
 		} while (digitarOutro == 'S');
+
+		do {
+			System.out.printf("%nMENU%n");
+			System.out.println("1 - Total de horas trabalhadas");
+			System.out.println("2 - Custo total");
+			System.out.println("3 - Nome da pessoa que ganhou mais");
+			System.out.println("4 - Sair");
+			System.out.printf("Digite uma opção: ");
+			opcao = sc.nextInt();
+			
+		} while (opcao != 4);
 
 		sc.close();
 
